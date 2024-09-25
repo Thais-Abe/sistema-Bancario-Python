b301aa9c6c43c273cf038fcc16de9c53b05afe9d
Thais-Abe <abe.c.thais@gmail.com>
2024-09-25 09:26:51 -0300

    solution of challenge

--- challenge.py
+++ challenge.py
@@ -1,9 +1,9 @@
 menu = """
 
 [d] Depositar
-[s] Sacar 
+[s] Sacar
 [e] Extrato
-[x] Sair 
+[q] Sair
 
 => """
 
@@ -13,16 +13,56 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-while True: 
+
+
+while True:
+
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
-    elif opcao =='s':
-        print("Saque")
-    elif opcao =="e":
-        print("Extrato")
-    elif opcao =="x":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Informe um valor válido.")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "q":
         break
+
     else:
-        print("Operação inválida, por favor seleciona novamente a operação desejada.")
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
