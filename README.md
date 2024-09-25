3c4a823d7ee2e1cdd007df08effca47512fb5247
Thais-Abe <abe.c.thais@gmail.com>
2024-09-25 09:48:03 -0300

    adjusts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sistema-Bancario-Python
-Projeto de um projeto bancário em Python. O objetivo é implementar três operações essenciais: depósito, saque e extrato. 
+
+Projeto de um projeto bancário em Python. O objetivo é implementar três operações essenciais: depósito, saque e extrato.
--- challenge.py
+++ challenge.py
@@ -2,67 +2,60 @@ menu = """
 
 [d] Depositar
 [s] Sacar
-[e] Extrato
+[e] extract
 [q] Sair
 
 => """
 
-saldo = 0
+balance = 0
 limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
-
+extract = ""
+number_sakes = 0
+LIMITE_SAKES = 3
 
 
 while True:
 
-    opcao = input(menu)
+    option = input(menu)
 
-    if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+    if option == "d":
+        value = float(input("Informe o value do depósito: "))
 
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f} ------------------ \n"
-
-        else:
-            print("Informe um valor válido.")
-
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
-
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Saldo insuficiente.")
-
-        elif excedeu_limite:
-            print(" O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Número máximo de saques excedido.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
+        if value > 0:
+            balance += value
+            extract += f"Depósito: R$ {value:.2f} ------------------ \n"
 
         else:
-            print("O valor informado é inválido.")
+            print("Informe um value válido.")
 
-    elif opcao == "e":
-        print("\n================ EXTRATO ================")
+    elif option == "s":
+        value = float(input("Informe o value do sake: "))
+
+        if value > balance:
+            print("balance insuficiente.")
+
+        elif value > limite:
+            print(" O value do sake excede o limite.")
+
+        elif number_sakes >= LIMITE_SAKES:
+            print("Número máximo de sakes excedido.")
+
+        elif value > 0:
+            balance -= value
+            extract += f"sake: R$ {value:.2f}\n"
+            number_sakes += 1
+
+        else:
+            print("O value informado é inválido.")
+
+    elif option == "e":
+        print("\n================ extract ================")
         print("\n")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("Não foram realizadas movimentações." if not extract else extract)
+        print(f"\nbalance: R$ {balance:.2f}")
         print("==========================================")
 
-    elif opcao == "q":
+    elif option == "q":
         break
 
     else:
